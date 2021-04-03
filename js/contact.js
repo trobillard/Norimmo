120bbef6b229590014aba8edf0c58d7c443e0cb0
samir <samir.bas24@gmail.com>
2021-04-03 16:15:49 +0200

    5 commit

--- js/contact.js
+++ js/contact.js
@@ -4,16 +4,20 @@
 //     // alert("coucou");
 //     event.target.style.innerText = "black";
 // })
+// let btn = document.querySelector('button[type="submit"]');
+// btn.onclick = function() {
+//     console.log('sss');
+// }
 
 function checkForm() {
-    let lastName = document.getElementById("lastName");
-    if (lastName.value == '') {
+ 
+   if (lastName.value == '' || lastName.value.length < 2) {
         let errorLastNameme = document.getElementById("errorLastName");
-        errorLastName.innerText = "Attention Le nom est obligatoire"
+        errorLastName.innerText = "Attention Le nom est obligatoire et doit contenir au moins 2 caractères"
         lastName.focus();
         lastName.className += " box-red";
         return false;
-    }   
+    }
     else {
         lastName.className = "form-control form-control:focus"
         let errorLastNameme = document.getElementById("errorLastName");
@@ -21,9 +25,9 @@ function checkForm() {
     }
  
     let firstName = document.getElementById("firstName");
-    if (firstName.value == '') {
+    if (firstName.value == '' || firstName.value.length < 2) {
         let errorFirstName = document.getElementById("errorFirstName");
-        errorFirstName.innerText = "Attention Le prénom est obligatoire"
+        errorFirstName.innerText = "Attention Le prénom est obligatoire et doit contenir au moins 2 caractères"
         firstName.focus();
         firstName.className += " box-red";
         return false;
@@ -49,7 +53,7 @@ function checkForm() {
     }
 
     let message = document.getElementById("message");
-    alert(message.value);
+    // alert(message.value);
     if (message.value === '') {
         let errorMessage = document.getElementById("errorMessage");
         errorMessage.innerText = "Attention Le Message est obligatoire"
@@ -58,17 +62,68 @@ function checkForm() {
         return false;
     }
     else {
-        errorMessage.className = "form-control form-control:focus"
+        message.className = "form-control form-control:focus"
         let errorMessage = document.getElementById("errorMessage");
         errorMessage.innerText = ""
     }
+
     return true;
 }
 
+let words = ["sexe", "sex", "con", "connard"];
+function textArea(max) {
+    let reste;
+    let message = document.getElementById("message")
+    let errorMessage = document.getElementById("errorMessage");
+    errorMessage.innerText = max + " Caractère(s) restant(s)"
+    message.onkeypress = function () {
+        reste = max - message.value.length
+        if (reste < 1) {
+            return false;
+        }
+        errorMessage.innerText = reste + " Caractère(s) restant(s)"
+    }
+    message.onkeyup = function () {
+        reste = max - message.value.length
+        if (reste < 0) {
+            return false;
+        }
+        errorMessage.innerText = reste + " Caractère(s) restant(s)"
 
+        if (message.value !== '') {
+            for (let i=0 ; i < words.length; i++) {
+                if (message.value.indexOf(words[i]) > -1) {
+                    message.className += " box-red";
+                    break;
+                }
+                else {
+                    message.className = "form-control form-control:focus"
+                }
+            }
+        }
+    }
+}
 
-// let btn = document.querySelector('button[type="submit"]');
-// btn.onclick = function() {
-//     console.log('sss');
-// }
+textArea(400);
 
+function checkLastname() {
+    let lastName = document.getElementById("lastName");
+    lastName.addEventListener("keypress", function(event) {
+        if ((event.keyCode >= 48 && event.keyCode <= 57) || lastName.value.length >= 50) {
+            event.preventDefault();
+        }
+    });
+}
+
+checkLastname();
+
+function checkFirstname() {
+    let firstName = document.getElementById("firstName");
+    firstName.addEventListener("keypress", function(event) {
+        if ((event.keyCode >= 48 && event.keyCode <= 57) || firstName.value.length >= 50) {
+            event.preventDefault();
+        }
+    });
+}
+
+checkFirstname();
